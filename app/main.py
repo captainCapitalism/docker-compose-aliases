9fbaa26fdad6268eab185a98eb75bd789527856e
michal bargiel <michal5bargiel@gmail.com>
2021-12-31 00:04:51 +0100

    ✨ Add basic docker commands.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,31 @@
 import typer
+import os
 
 app = typer.Typer()
 
 
 @app.command()
-def hello():
-    typer.echo("HELLO")
+def up(build: bool = False):
+    options = ""
+    if build:
+        options = f"{options} --build"
+
+    os.system(f"docker-compose up -d{options}")
 
 
+@app.command()
+def down():
+    os.system("docker-compose down")
+
+
+@app.command()
+def reset(build: bool = False):
+    down()
+    up(build)
+
+
+app.command(name="u")(up)
+app.command(name="d")(down)
+app.command(name="r")(reset)
 if __name__ == "__main__":
     app()
