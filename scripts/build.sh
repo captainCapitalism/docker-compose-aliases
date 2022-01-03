733148ac61925fcdd42f56b8c0f3f9455e2aa829
michal bargiel <michal5bargiel@gmail.com>
2022-01-03 22:57:47 +0100

    ♻️ Update so won't fail when main project is running.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 set -e
 
-docker-compose -f docker-compose-build.yaml up --build -d
-docker-compose -f docker-compose-build.yaml down
+docker-compose -p docker-compose-aliases-build -f docker-compose-build.yaml up --build -d
+docker-compose -p docker-compose-aliases-build -f docker-compose-build.yaml down
 
 sudo chmod -R 777 dist
